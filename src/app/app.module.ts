8d0cab575a2a4c679e3556fc3ac6ae0f6a24734e
mavlopp <alaric.smith@outlook.fr>
2022-02-11 17:25:23 +0100

    add component listperson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListpersonComponent } from './listperson/listperson.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListpersonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/listperson/listperson.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listperson',
+  templateUrl: './listperson.component.html',
+  styleUrls: ['./listperson.component.css']
+})
+export class ListpersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
